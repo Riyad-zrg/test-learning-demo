e8f02a23cced4888fad348767a2d6c72ed3e3593
zerr0009 <riyad.zerrougui@etudiant.univ-reims.fr>
2025-12-20 10:02:17 +0100

    feat(CommentTest): test constructor publishedAt initialization

--- tests/Unit/CommentTest.php
+++ tests/Unit/CommentTest.php
@@ -17,7 +17,6 @@ class CommentTest extends \Codeception\Test\Unit
     {
     }
 
-    // On peut vérifier isLegitComment avec bonne et mauvaises valeur et vérifier que publishedAt existe bien
     public function testIsLegitComment()
     {
         //Créer deux commentaires, un valide et un invalide, vérifier que les événements attendus se produisent
@@ -29,6 +28,15 @@ class CommentTest extends \Codeception\Test\Unit
 
         $comment->setContent("@@@@@@@");
         $this->assertFalse($comment->isLegitComment(),"Le commentaire est invalide");
+    }
+
+    public function testPublishedAt()
+    {
+        //Vérifie que quand un commentaire est créé, l'attribut publishedAt est correctement initialisé
+
+        $comment = new Comment();
+        $this->assertTrue($comment->getPublishedAt() instanceof \DateTimeImmutable);
+
     }
 
 
